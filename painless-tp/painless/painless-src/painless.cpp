bb1214a342ade9ec17641909ba75b45f8545820b
RobinMayo <rbmayo@laposte.net>
2019-03-01 01:45:32 +0100

    Question 4

--- painless-tp/painless/painless-src/painless.cpp
+++ painless-tp/painless/painless-src/painless.cpp
@@ -57,199 +57,208 @@ vector<int> finalModel;
 // -------------------------------------------
 int main(int argc, char ** argv)
 {
-   Parameters::init(argc, argv);
-   
-   if (Parameters::getFilename() == NULL ||
-       Parameters::isSet("h"))
-   {
-      printf("USAGE: %s [parameters] input.cnf\n", argv[0]);
-      printf("Parameters:\n");
-      printf("\t-solver=type\t\t glucose, maple, or combo, default maple\n");
-      printf("\t-lbd-limit=<INT>\t lbd limit of exported clauses\n");
-      printf("\t-d=0...7\t\t diversification 0=none, 1=sparse, 2=dense," \
-             " 3=random, 4=native, 5=1&4, 6=sparse-random, 7=6&4," \
-             " default is 0.\n");
-      printf("\t-c=<INT>\t\t number of cpus, default is 4.\n");
-      printf("\t-wkr-strat=1...2\t 1=divide and conquer (DC), 2=portfolio" \
-             " (PF), 3=PF of DC, default is DC");
-      printf("\t-shr-strat=0...1\t 0=no sharing, 1=simple sharing, default is" \
-             " 0\n");
-      printf("\t-shr-sleep=<INT>\t time in usecond a sharer sleep each" \
-             " round, default 500000 (0.5s)\n");
-      printf("\t-no-model\t\t won't print the model if the problem is SAT\n");
-      printf("\t-t=<INT>\t\t timeout in second, default is no limit\n");
-      printf("\t-split-heur=1...3\t for D&C: splitting heuristic," \
-	          " 1=VSIDS, 2=#flips, 3=propagation rate, default is 1\n");
-      printf("\t-copy-mode=1...2\t for D&C: method to allocate new subspaces " \
-             "to workers, 1=no copy: reuses the old solver, 2=clone: clones " \
-             "the solver and delete old solver, default is 1\n");
-
-      return 0;
-   }
-
-   log(2, "CNF file found !\n");
-   Parameters::printParams();
-
-   setVerbosityLevel(Parameters::getIntParam("v", 0));
-
-   int cpus = Parameters::getIntParam("c", 4);
-
-   srand(time(NULL));
-
-   // Create solvers
-   log(1, "painless.cpp\t - Create solvers\n");
-   vector<SolverInterface *> solvers;
-   
-   const string solverType = Parameters::getParam("solver");
-   const int wkrStrat      = Parameters::getIntParam("wkr-strat", 1);
-
-   int nSolvers = cpus;
-   if (wkrStrat == 1 && Parameters::getIntParam("copy-mode", 1) == 2) {
-       nSolvers = 1;
-   }
-
-   if (solverType == "glucose") {
-      SolverFactory::createGlucoseSolvers(nSolvers, solvers);
-   } else if(solverType == "combo") {
-      SolverFactory::createComboSolvers(nSolvers, solvers);
-   } else {
-      SolverFactory::createMapleSolvers(nSolvers, solvers);
-   }
-
-   // Diversifycation
-   log(1, "painless.cpp\t - Diversification\n");
-   int diversification = Parameters::getIntParam("d", 0);
-
-   switch (diversification) {
-      case 1 :
-         SolverFactory::sparseDiversification(solvers);
-         break;
-
-      case 2 :
-         SolverFactory::binValueDiversification(solvers);
-         break;
-
-      case 3 :
-         SolverFactory::randomDiversification(solvers, 2015);
-         break;
-
-      case 4 :
-         SolverFactory::nativeDiversification(solvers);
-         break;
-
-      case 5 :
-         SolverFactory::sparseDiversification(solvers);
-         SolverFactory::nativeDiversification(solvers);
-         break;
-
-      case 6 :
-         SolverFactory::sparseRandomDiversification(solvers);
-         break;
-
-      case 7 :
-         SolverFactory::sparseRandomDiversification(solvers);
-         SolverFactory::nativeDiversification(solvers);
-         break;
-
-      case 0 :
-         break;
-   }
-
-   // Start sharing threads
-   log(1, "painless.cpp\t - Start sharing threads\n");
-   switch(Parameters::getIntParam("shr-strat", 0)) {
-      case 1 :
-         nSharers   = 1;
-         sharers    = new Sharer*[nSharers];
-         sharers[0] = new Sharer(0, new SimpleSharing(), solvers, solvers);
-         break;
-
-      case 0 :
-      default:
-         break;
-   }
-
-   // Working strategy creation
-   log(1, "painless.cpp\t - Working strategy creation\n");
-   switch(wkrStrat) {
-      case 2 :
-         working = new Portfolio();
-         for (size_t i = 0; i < cpus; i++) {
-            working->addSlave(new SequentialWorker(solvers[i]));
-         }
-         break;
-
-      case 1 :
-      default :
-         working = new DivideAndConquer();
-         if(Parameters::getIntParam("copy-mode",1) == 2) {
-            working->addSlave(new SequentialWorker(solvers[0]));
-            for(size_t i = 1; i < cpus; i++) {
-	            working->addSlave(new SequentialWorker(NULL));
-            }
-         } else {
-            for(size_t i = 0; i < cpus; i++) {
-	            working->addSlave(new SequentialWorker(solvers[i]));
-            }
-         }
-         break;
-   }
-
-   // Init the management of clauses
-   log(1, "painless.cpp\t - initClauseManager()\n");
-   ClauseManager::initClauseManager();
-
-   // Launch working
-   log(1, "painless.cpp\t - Launch working\n");
-   vector<int> cube;
-   working->solve(cube);
-
-   // Wait until end or timeout
-   log(1, "painless.cpp\t - Wait until end or timeout\n");
-   int timeout   = Parameters::getIntParam("t", -1);
-   int maxMemory = Parameters::getIntParam("max-memory", -1) * 1024 * 1024;
-
-   while(globalEnding == false) {
-      sleep(1);
-
-      if (maxMemory > 0 && getMemoryUsed() > maxMemory) {
-         cout << "c Memory used is going too large!!!!" << endl;
-      }
-
-      if (timeout > 0 && getRelativeTime() >= timeout) {
-         globalEnding = true;
-         working->setInterrupt();
-      }
-   }
-
-   // Delete sharers
-   log(1, "painless.cpp\t - Delete sharers\n");
-   for (int i=0; i < nSharers; i++) {
-      delete sharers[i];
-   }
-   delete sharers;
-
-   // Delete working strategy
-   delete working;
-
-   // Delete shared clauses
-   ClauseManager::joinClauseManager();
-
-   // Print solver stats
-   SolverFactory::printStats(solvers);
-
-   // Print the result and the model if SAT
-   if (finalResult == SAT) {
-      printf("s SATISFIABLE\n");
-
-      if (Parameters::isSet("no-model") == false) {
-         printModel(finalModel);
-      }
-   } else if (finalResult == UNSAT) {
-      printf("s UNSATISFIABLE\n");
-   } else {
-      printf("s UNKNOWN\n");
-   }
-
-   return finalResult;
+	Parameters::init(argc, argv);
+	
+	if (Parameters::getFilename() == NULL ||
+		Parameters::isSet("h"))
+	{
+		printf("USAGE: %s [parameters] input.cnf\n", argv[0]);
+		printf("Parameters:\n");
+		printf("\t-solver=type\t\t glucose, maple, or combo, default maple\n");
+		printf("\t-lbd-limit=<INT>\t lbd limit of exported clauses\n");
+		printf("\t-d=0...7\t\t diversification 0=none, 1=sparse, 2=dense," \
+			   " 3=random, 4=native, 5=1&4, 6=sparse-random, 7=6&4," \
+			   " default is 0.\n");
+		printf("\t-c=<INT>\t\t number of cpus, default is 4.\n");
+		printf("\t-wkr-strat=1...2\t 1=divide and conquer (DC), 2=portfolio" \
+			   " (PF), 3=PF of DC, default is DC");
+		printf("\t-shr-strat=0...1\t 0=no sharing, 1=simple sharing, default is" \
+			   " 0\n");
+		printf("\t-shr-sleep=<INT>\t time in usecond a sharer sleep each" \
+			   " round, default 500000 (0.5s)\n");
+		printf("\t-no-model\t\t won't print the model if the problem is SAT\n");
+		printf("\t-t=<INT>\t\t timeout in second, default is no limit\n");
+		printf("\t-split-heur=1...3\t for D&C: splitting heuristic," \
+			   " 1=VSIDS, 2=#flips, 3=propagation rate, default is 1\n");
+		printf("\t-copy-mode=1...2\t for D&C: method to allocate new subspaces " \
+			   "to workers, 1=no copy: reuses the old solver, 2=clone: clones " \
+			   "the solver and delete old solver, default is 1\n");
+		
+		return 0;
+	}
+	
+	log(2, "CNF file found !\n");
+	Parameters::printParams();
+	
+	setVerbosityLevel(Parameters::getIntParam("v", 0));
+	
+	int cpus = Parameters::getIntParam("c", 4);
+	
+	srand(time(NULL));
+	
+	// Create solvers
+	log(1, "painless.cpp\t - Create solvers\n");
+	vector<SolverInterface *> solvers;
+	
+	const string solverType = Parameters::getParam("solver");
+	const int wkrStrat      = Parameters::getIntParam("wkr-strat", 1);
+	
+	int nSolvers = cpus;
+	if (wkrStrat == 1 && Parameters::getIntParam("copy-mode", 1) == 2) {
+		nSolvers = 1;
+	}
+	
+	if (solverType == "glucose") {
+		SolverFactory::createGlucoseSolvers(nSolvers, solvers);
+	} else if(solverType == "combo") {
+		SolverFactory::createComboSolvers(nSolvers, solvers);
+	} else {
+		SolverFactory::createMapleSolvers(nSolvers, solvers);
+	}
+	
+	// Diversifycation
+	log(1, "painless.cpp\t - Diversification\n");
+	int diversification = Parameters::getIntParam("d", 0);
+	
+	switch (diversification) {
+		case 1 :
+			SolverFactory::sparseDiversification(solvers);
+			break;
+			
+		case 2 :
+			SolverFactory::binValueDiversification(solvers);
+			break;
+			
+		case 3 :
+			SolverFactory::randomDiversification(solvers, 2015);
+			break;
+			
+		case 4 :
+			SolverFactory::nativeDiversification(solvers);
+			break;
+			
+		case 5 :
+			SolverFactory::sparseDiversification(solvers);
+			SolverFactory::nativeDiversification(solvers);
+			break;
+			
+		case 6 :
+			SolverFactory::sparseRandomDiversification(solvers);
+			break;
+			
+		case 7 :
+			SolverFactory::sparseRandomDiversification(solvers);
+			SolverFactory::nativeDiversification(solvers);
+			break;
+			
+		case 0 :
+			break;
+	}
+	
+	// Start sharing threads
+	log(1, "painless.cpp\t - Start sharing threads\n");
+	switch(Parameters::getIntParam("shr-strat", 0)) {
+		case 1 :
+			nSharers   = 1;
+			sharers    = new Sharer*[nSharers];
+			sharers[0] = new Sharer(0, new SimpleSharing(), solvers, solvers);
+			break;
+			
+		case 0 :
+		default:
+			break;
+	}
+	
+	// Working strategy creation
+	log(1, "painless.cpp\t - Working strategy creation\n");
+	switch(wkrStrat) {
+		// Question 4 :
+		case 3 :
+			working = new Portfolio();
+			
+			for (size_t i = 0; i < cpus; i++) {
+				working->addSlave(new SequentialWorker(solvers[i]));
+			}
+			break;
+		// END question 4.
+		case 2 :
+			working = new Portfolio();
+			for (size_t i = 0; i < cpus; i++) {
+				working->addSlave(new SequentialWorker(solvers[i]));
+			}
+			break;
+			
+		case 1 :
+		default :
+			working = new DivideAndConquer();
+			if(Parameters::getIntParam("copy-mode",1) == 2) {
+				working->addSlave(new SequentialWorker(solvers[0]));
+				for(size_t i = 1; i < cpus; i++) {
+					working->addSlave(new SequentialWorker(NULL));
+				}
+			} else {
+				for(size_t i = 0; i < cpus; i++) {
+					working->addSlave(new SequentialWorker(solvers[i]));
+				}
+			}
+			break;
+	}
+	
+	// Init the management of clauses
+	log(1, "painless.cpp\t - initClauseManager()\n");
+	ClauseManager::initClauseManager();
+	
+	// Launch working
+	log(1, "painless.cpp\t - Launch working\n");
+	vector<int> cube;
+	working->solve(cube);
+	
+	// Wait until end or timeout
+	log(1, "painless.cpp\t - Wait until end or timeout\n");
+	int timeout   = Parameters::getIntParam("t", -1);
+	int maxMemory = Parameters::getIntParam("max-memory", -1) * 1024 * 1024;
+	
+	while(globalEnding == false) {
+		sleep(1);
+		
+		if (maxMemory > 0 && getMemoryUsed() > maxMemory) {
+			cout << "c Memory used is going too large!!!!" << endl;
+		}
+		
+		if (timeout > 0 && getRelativeTime() >= timeout) {
+			globalEnding = true;
+			working->setInterrupt();
+		}
+	}
+	
+	// Delete sharers
+	log(1, "painless.cpp\t - Delete sharers\n");
+	for (int i=0; i < nSharers; i++) {
+		delete sharers[i];
+	}
+	delete sharers;
+	
+	// Delete working strategy
+	delete working;
+	
+	// Delete shared clauses
+	ClauseManager::joinClauseManager();
+	
+	// Print solver stats
+	SolverFactory::printStats(solvers);
+	
+	// Print the result and the model if SAT
+	if (finalResult == SAT) {
+		printf("s SATISFIABLE\n");
+		
+		if (Parameters::isSet("no-model") == false) {
+			printModel(finalModel);
+		}
+	} else if (finalResult == UNSAT) {
+		printf("s UNSATISFIABLE\n");
+	} else {
+		printf("s UNKNOWN\n");
+	}
+	
+	return finalResult;
 }
